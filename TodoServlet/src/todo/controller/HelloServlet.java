929174b4f589ab68225776d0b80d1b6ae7dbfac9
uqw <uqw@outlook.jp>
2019-01-30 11:29:19 +0900

    Add project

---
+++ TodoServlet/src/todo/controller/HelloServlet.java
@@ -0,0 +1,65 @@
+package todo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	void debug() {
+		System.out.println("aaaaaaaaaaaaaaaaaaaaasaaaaaa");
+	}
+
+	void debug(String str) {
+		System.out.println("aaaaaaaaaaaaaaaaaaaaasaaaaaa:" + str);
+	}
+
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		debug("get");
+
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		 String view = "/WEB-INF/view/index.jsp";
+		 RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+
+		 dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		debug("postt");
+
+		doGet(request, response);
+	}
+
+}
